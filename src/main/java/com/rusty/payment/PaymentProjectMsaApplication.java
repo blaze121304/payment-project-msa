e42479f856c2455df7234c0db19072c6b10c01df
rusty <blaze121304@gmail.com>
2024-11-27 14:30:41 +0900

    2024.11.27
    payment-project-msa
    
    1. 아키텍쳐 커밋

---
+++ src/main/java/com/rusty/payment/PaymentProjectMsaApplication.java
@@ -0,0 +1,13 @@
+package com.rusty.payment;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PaymentProjectMsaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PaymentProjectMsaApplication.class, args);
+	}
+
+}
